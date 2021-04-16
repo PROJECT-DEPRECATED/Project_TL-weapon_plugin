686736cf4bf38092424436a5ec7597c9edbd5305
ProjectTL12345 <kimpiao020@gmail.com>
2021-04-16 09:07:26 +0900

    Update: Fix getter item and Add event

--- src/main/kotlin/org/projecttl/plugin/weapon/WeaponPlugin.kt
+++ src/main/kotlin/org/projecttl/plugin/weapon/WeaponPlugin.kt
@@ -3,16 +3,23 @@ package org.projecttl.plugin.weapon
 import org.bukkit.configuration.file.FileConfiguration
 import org.bukkit.configuration.file.YamlConfiguration
 import org.bukkit.plugin.java.JavaPlugin
+import org.projecttl.plugin.weapon.commands.SpawnWeapon
+import org.projecttl.plugin.weapon.listeners.PistolListener
 import java.io.File
 
 class WeaponPlugin: JavaPlugin() {
 
     private var getFile: File? = null
     private var configuration: FileConfiguration? = null
+    private var manager = server.pluginManager
 
     override fun onEnable() {
         load()
         logger.info("Plugin enabled!")
+
+        getCommand("weapon")?.setExecutor(SpawnWeapon())
+
+        manager.registerEvents(PistolListener(), this)
     }
 
     override fun onDisable() {
--- src/main/kotlin/org/projecttl/plugin/weapon/listeners/PistolListener.kt
+++ src/main/kotlin/org/projecttl/plugin/weapon/listeners/PistolListener.kt
@@ -1,5 +1,9 @@
 package org.projecttl.plugin.weapon.listeners
 
+import org.bukkit.Effect
+import org.bukkit.Sound
+import org.bukkit.entity.Arrow
+import org.bukkit.entity.Projectile
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.block.Action
@@ -16,10 +20,21 @@ class PistolListener: Listener {
         val playerMainHand = player.inventory.itemInMainHand
 
         if (action == Action.RIGHT_CLICK_AIR && action == Action.RIGHT_CLICK_BLOCK) {
-            if (player.name == "Project_TL") {
-                if (playerMainHand.type == Pistol.itemStack().type) {
-                    if (playerMainHand.itemMeta.displayName == Pistol.getItemName()) {
+            if (player.name == "Project_TL" && playerMainHand.type == Pistol.itemStack().type) {
+                if (playerMainHand.itemMeta.displayName == Pistol.getItemName() && playerMainHand.itemMeta.customModelData == Pistol.getCustomModelData()) {
+                    val bullet: Projectile = player.launchProjectile(Arrow::class.java).let { bullet ->
+                        bullet.velocity = player.location.direction.multiply(1.5)
+                        bullet.world.playEffect(bullet.location, Effect.SMOKE, 10)
+
+                        bullet
                     }
+
+                    bullet.world.playSound(
+                        player.location,
+                        Sound.ENTITY_FIREWORK_ROCKET_LARGE_BLAST,
+                        100.toFloat(),
+                        1.toFloat()
+                    )
                 }
             }
         }
