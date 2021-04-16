8224b9e5ba79ae4571a455555cfabcde361352fb
ProjectTL12345 <kimpiao020@gmail.com>
2021-04-17 00:25:14 +0900

    Update: Fix bugs #2

--- src/main/kotlin/org/projecttl/plugin/weapon/utils/Skills.kt
+++ src/main/kotlin/org/projecttl/plugin/weapon/utils/Skills.kt
@@ -39,7 +39,7 @@ class Skills(private val plugin: WeaponPlugin) {
                     plugin.weaponConfig().set("weapon.cooldown", false)
                     player.sendMessage("<Skill_Manager> ${ChatColor.GOLD}You're cooldown is end!")
                 }
-            }.runTaskLater(plugin, cooldownTime.toLong())
+            }.runTaskLater(plugin, cooldownTime.toLong() * 20)
 
         } else {
             player.sendMessage("<Skill_Manager> ${ChatColor.GOLD}You can use the skill $cooldownTime seconds from the time you use it.")
