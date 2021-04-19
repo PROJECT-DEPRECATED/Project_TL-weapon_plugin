cc8ea867a90b973a7cad4144eb1880b38f5ef9f9
ProjectTL12345 <kimpiao020@gmail.com>
2021-04-19 21:08:06 +0900

    Update: DEBUG

--- src/main/kotlin/org/projecttl/plugin/weapon/listeners/PistolListener.kt
+++ src/main/kotlin/org/projecttl/plugin/weapon/listeners/PistolListener.kt
@@ -16,79 +16,55 @@ import org.projecttl.plugin.weapon.WeaponPlugin
 import org.projecttl.plugin.weapon.utils.Pistol
 
 
-class PistolListener(private var plugin: WeaponPlugin): Listener {
+class PistolListener(private val plugin: WeaponPlugin): Listener {
 
-    private val reloading = plugin.weaponConfig().getBoolean("weapon.projecttl.pistol.reload")
-    private val leftAmmo = plugin.weaponConfig().getInt("weapon.projecttl.pistol.leftAmmo")
+    private val shootingPath = "weapon.projecttl.pistol.shooting"
+    private val shooting = plugin.weaponConfig().getBoolean(shootingPath)
+
+    private val reloadingPath = "weapon.projecttl.pistol.reload"
+    private val reloading = plugin.weaponConfig().getBoolean(reloadingPath)
+
+    private val leftAmmoPath = "weapon.projecttl.pistol.leftAmmo"
+    private val leftAmmo = plugin.weaponConfig().getInt(leftAmmoPath)
+
     private val fixAmmoCount = 12
 
     @EventHandler
     fun onEvent(event: PlayerInteractEvent) {
         val player = event.player
-        val action = event.action
 
-        val playerMainHand = player.inventory.itemInMainHand
-
-        when (action) {
+        when (event.action) {
             Action.LEFT_CLICK_AIR, Action.LEFT_CLICK_BLOCK -> {
-                if (player.name == WeaponPlugin.target && playerMainHand.type == Pistol.itemStack().type) {
-                    if (playerMainHand.itemMeta.displayName == Pistol.getItemName() && playerMainHand.itemMeta.customModelData == Pistol.getCustomModelData()) {
-                        when {
-                            leftAmmo > 0 -> {
-                                shoot(player)
+                if (leftAmmo != 0) {
+                    if (!shooting) {
+                        plugin.weaponConfig().set(shootingPath, true)
+                        val bullet: Projectile = player.launchProjectile<Projectile>(Snowball::class.java).let { bullet ->
+                                bullet.velocity = player.location.direction.multiply(5)
+
+                                bullet
                             }
 
-                            else -> {
-                                player.playSound(player.location, Sound.BLOCK_IRON_DOOR_CLOSE, 100.toFloat(), 2.toFloat())
-                                player.sendActionBar("${ChatColor.GOLD}Left Bullet: ${ChatColor.RED}$leftAmmo${ChatColor.GREEN}/$fixAmmoCount")
-                            }
-                        }
+                        bullet.world.playEffect(player.location, Effect.SMOKE, 10)
+                        bullet.world.playSound(
+                            bullet.location,
+                            Sound.ENTITY_FIREWORK_ROCKET_BLAST,
+                            100.toFloat(),
+                            1.toFloat()
+                        )
+
+                        plugin.weaponConfig().set(leftAmmoPath, leftAmmo - 1)
+                        player.sendActionBar("${ChatColor.GOLD}Left Bullet: ${ChatColor.GREEN}${leftAmmo}/${fixAmmoCount}")
+
+                        Bukkit.getScheduler().runTaskLater(plugin, Runnable {
+                            plugin.weaponConfig().set(shootingPath, false)
+                        }, (0.7 * 20).toLong())
                     }
+
                 }
             }
 
             Action.RIGHT_CLICK_AIR -> {
-                if (player.name == WeaponPlugin.target && playerMainHand.type == Pistol.itemStack().type) {
-                    if (playerMainHand.itemMeta.displayName == Pistol.getItemName() && playerMainHand.itemMeta.customModelData == Pistol.getCustomModelData()) {
-                        when (leftAmmo) {
-                            fixAmmoCount -> {
-                                player.sendActionBar("${ChatColor.GOLD}Left Bullet: ${ChatColor.GREEN}$leftAmmo/$fixAmmoCount")
-                                player.playSound(player.location, Sound.BLOCK_IRON_DOOR_CLOSE, 100.toFloat(), 2.toFloat())
-                            }
-                            else -> {
-                                reload(player)
-                            }
-                        }
-                    }
-                }
-            }
-
-            Action.RIGHT_CLICK_BLOCK -> {
-                if (player.name == WeaponPlugin.target && playerMainHand.type == Pistol.itemStack().type) {
-                    if (playerMainHand.itemMeta.displayName == Pistol.getItemName() && playerMainHand.itemMeta.customModelData == Pistol.getCustomModelData()) {
-                        when {
-                            leftAmmo == fixAmmoCount -> {
-                                player.sendActionBar("${ChatColor.GOLD}Left Bullet: ${ChatColor.GREEN}$leftAmmo/$fixAmmoCount")
-                                player.playSound(player.location, Sound.BLOCK_IRON_DOOR_CLOSE, 100.toFloat(), 2.toFloat())
-                            }
-
-                            leftAmmo <= 1 -> {
-                                player.sendActionBar("${ChatColor.GOLD}Left Bullet: ${ChatColor.GOLD}$leftAmmo/$fixAmmoCount")
-                                player.playSound(player.location, Sound.BLOCK_IRON_DOOR_CLOSE, 100.toFloat(), 2.toFloat())
-                            }
-
-                            else -> {
-                                player.sendActionBar("${ChatColor.GOLD}Left Bullet: ${ChatColor.RED}$leftAmmo/$fixAmmoCount")
-                                player.playSound(player.location, Sound.BLOCK_IRON_DOOR_CLOSE, 100.toFloat(), 2.toFloat())
-                            }
-                        }
-                    }
-
-                    event.isCancelled = true
-                }
-            }
-            else -> {
-                // void
+                plugin.weaponConfig().set(leftAmmoPath, fixAmmoCount)
             }
         }
     }
@@ -108,62 +84,6 @@ class PistolListener(private var plugin: WeaponPlugin): Listener {
                         event.damage = 5.0
                     }
                 }
-            }
-        }
-    }
-
-    private fun shoot(player: Player) {
-        val bullet: Projectile = player.launchProjectile(Snowball::class.java).let { bullet ->
-            bullet.velocity = player.location.direction.multiply(5)
-            bullet
-        }
-
-        bullet.world.playEffect(bullet.location, Effect.SMOKE, 10)
-        bullet.world.playSound(
-            player.location,
-            Sound.ENTITY_FIREWORK_ROCKET_LARGE_BLAST,
-            100.toFloat(),
-            1.toFloat()
-        )
-
-        plugin.weaponConfig().set("weapon.projecttl.pistol.leftAmmo", leftAmmo - 1)
-        player.sendActionBar("${ChatColor.GOLD}Left Bullet: ${ChatColor.GREEN}${leftAmmo}/${fixAmmoCount}")
-    }
-
-    private fun reload(player: Player) {
-        when {
-            !reloading -> {
-                player.sendActionBar("${ChatColor.GOLD}Reloading...")
-                player.playSound(
-                    player.location,
-                    Sound.BLOCK_IRON_DOOR_OPEN,
-                    100.toFloat(),
-                    2.toFloat()
-                )
-
-                plugin.weaponConfig().set("weapon.projecttl.pistol.reload", true)
-
-                Bukkit.getScheduler().runTaskLater(plugin, Runnable {
-                    player.playSound(
-                        player.location,
-                        Sound.BLOCK_IRON_DOOR_CLOSE,
-                        100.toFloat(),
-                        2.toFloat()
-                    )
-
-                    plugin.weaponConfig().set("weapon.projecttl.pistol.leftAmmo", 12)
-                    plugin.weaponConfig().set("weapon.projecttl.pistol.reload", false)
-                }, (1.5 * 20).toLong())
-            }
-
-            else -> {
-                player.sendMessage("Rocket_Launcher> ${ChatColor.GOLD}You're already reloading!")
-                player.playSound(
-                    player.location,
-                    Sound.ENTITY_ENDERMAN_TELEPORT,
-                    100.toFloat(),
-                    1.0.toFloat()
-                )
             }
         }
     }
